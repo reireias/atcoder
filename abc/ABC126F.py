d45b97a10cb5bc36ef8447a3b6bd6ba75bd06126
reireias <reireias@gmail.com>
2019-05-21 22:18:41 +0900

    update ABC126F

--- abc/ABC126F.py
+++ abc/ABC126F.py
@@ -2,7 +2,26 @@
 
 
 def main():
-    pass
+    m, k = list(map(int, input().split()))
+    if k >= 2**m:
+        print(-1)
+        return
+    if m == 0:
+        if k == 0:
+            print('0 0')
+        else:
+            print(-1)
+        return
+    if m == 1:
+        if k == 0:
+            print('1 0 0 1')
+        else:
+            print(-1)
+        return
+    result = [i for i in range(2**m) if i != k]
+    b = list(map(str, result))
+    c = reversed(b)
+    print(' '.join(b), k, ' '.join(c), k)
 
 
 if __name__ == '__main__':
